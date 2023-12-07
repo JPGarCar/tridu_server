892af421aa9e61bea491a1e7133f2e9ac47c2e75
JP Garcia <juanpablo.garcia@alumni.ubc.ca>
2023-12-07 13:16:50 -0800

    Added more settings to NinjaAPI

--- tridu_server/api.py
+++ tridu_server/api.py
@@ -3,6 +3,8 @@ from ninja import NinjaAPI
 from tridu_server.api_security import GlobalJWTAuth
 
 jwt_auth = GlobalJWTAuth()
-api = NinjaAPI(auth=jwt_auth)
+api = NinjaAPI(
+    auth=jwt_auth, title="Tridu API", version="0.1", description="API for Tridu Server"
+)
 
 api.add_router("/users/", "accounts.api.router")
