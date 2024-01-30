5bde1045f4072bee4511a91e640dcdf078b43c9f
juanpablogarcia <juapgarc@gmail.com>
2024-01-30 09:12:20 -0800

    Endpoint to patch participant + schema.

--- participants/api.py
+++ participants/api.py
@@ -1,9 +1,16 @@
 from typing import List
 
-from ninja import Router
+from django.shortcuts import get_object_or_404
+from ninja import Router, Form
 
+from locations.models import Location
 from participants.models import Participant, ParticipantComment
-from participants.schema import ParticipantSchema, ParticipantCommentSchema
+from participants.schema import (
+    ParticipantSchema,
+    ParticipantCommentSchema,
+    ParticipantCommentCreateSchema,
+    PatchParticipantSchema,
+)
 
 router = Router()
 
@@ -22,6 +29,45 @@ def get_participants_for_user(request, user_id: int):
         return 201, participants
     else:
         return 204, None
+
+
+@router.patch(
+    "/{participant_id}", tags=["participants"], response={201: ParticipantSchema}
+)
+def update_participant(
+    request, participant_id: int, participantSchema: PatchParticipantSchema
+):
+    participant = get_object_or_404(Participant, pk=participant_id)
+
+    data = participantSchema.dict(exclude_unset=True)
+
+    # update origin of Participant
+    if "origin" in data:
+        origin_data = data.pop("origin", None)
+        if (
+            "country" in origin_data
+            and "province" in origin_data
+            and "city" in origin_data
+        ):
+            origin = Location.objects.filter(
+                country=origin_data["country"],
+                province=origin_data["province"],
+                city=origin_data["city"],
+            ).first()
+            if origin is None:
+                origin = Location.objects.create(
+                    country=origin_data["country"],
+                    province=origin_data["province"],
+                    city=origin_data["city"],
+                )
+            participant.origin = origin
+
+    # update participant values
+    for key, value in data.items():
+        setattr(participant, key, value)
+
+    participant.save()
+    return 201, participant
 
 
 @router.get(
--- participants/schema.py
+++ participants/schema.py
@@ -23,9 +23,24 @@ class ParticipantSchema(ModelSchema):
         )
 
 
+class PatchParticipantSchema(ModelSchema):
+    origin: LocationSchema
+
+    class Meta:
+        model = Participant
+        fields = ("id", "bib_number", "is_ftt", "team", "swim_time")
+        fields_optional = "__all__"
+
+
 class ParticipantCommentSchema(ModelSchema):
     writer_name: str = Field(None, alias="writer.first_name")
 
     class Meta:
         model = ParticipantComment
         fields = ("id", "participant", "comment", "creation_date")
+
+
+class ParticipantCommentCreateSchema(ModelSchema):
+    class Meta:
+        model = ParticipantComment
+        fields = ("comment",)
