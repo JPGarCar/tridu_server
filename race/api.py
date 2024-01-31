7ed61b1635f24ab804eb61a91e59b54f86ac519e
juanpablogarcia <juapgarc@gmail.com>
2024-01-30 16:08:29 -0800

    Race and Racetype endpoints only return active instances.

--- race/api.py
+++ race/api.py
@@ -10,9 +10,9 @@ router = Router()
 
 @router.get("/racetypes", tags=["racetypes"], response={200: List[RaceTypeSchema]})
 def get_race_types(request):
-    return 200, RaceType.objects.all()
+    return 200, RaceType.objects.filter(is_active=True).all()
 
 
 @router.get("/", tags=["races"], response={200: List[RaceSchema]})
 def get_races(request):
-    return 200, Race.objects.all()
+    return 200, Race.objects.filter(is_active=True).all()
