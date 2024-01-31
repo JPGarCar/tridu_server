e2148cd3877f304e7a8118031a94712ff4ba188f
juanpablogarcia <juapgarc@gmail.com>
2024-01-30 20:22:17 -0800

    Fixed heat api response.

--- heats/api.py
+++ heats/api.py
@@ -1,15 +1,14 @@
 from typing import List
 
-from django.contrib.sessions.models import Session
 from ninja import Router
 
 from heats.models import Heat
-from race.schema import RaceSchema
+from heats.schema import HeatSchema
 
 router = Router()
 
 
-@router.get("/race/{race_id}", tags=["heats"], response={200: List[RaceSchema]})
+@router.get("/race/{race_id}", tags=["heats"], response={200: List[HeatSchema]})
 def get_heats_for_race(request, race_id: int):
     heats = Heat.objects.filter(race_id=race_id).all()
     return 200, heats
