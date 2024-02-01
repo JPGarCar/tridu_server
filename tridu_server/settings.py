ddbe615d6534d6564744e6367272d9c08126d276
juanpablogarcia <juapgarc@gmail.com>
2024-02-01 15:37:56 -0800

    Added time zone to America/Vancouver

--- tridu_server/settings.py
+++ tridu_server/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+
 from datetime import timedelta
 from pathlib import Path
 
@@ -118,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Vancouver"
 
 USE_I18N = True
 
