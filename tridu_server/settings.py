9c749caed731a562637012078dc0dbb3b4c12c16
juanpablogarcia <juapgarc@gmail.com>
2025-02-07 15:28:34 -0800

    Added sentry support pt2

--- tridu_server/settings.py
+++ tridu_server/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 import sentry_sdk
 from dotenv import load_dotenv
-from sentry_sdk.integrations import DjangoIntegration
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
